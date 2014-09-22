ca2ed620d3f8432ee5e8e0cf717b225d52f5d6a0
Pavel Dvorkin <dpavel20@gmail.com>
2014-09-22 18:48:29 -0400

    Delete splinter.py

--- Listener/splinter.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'xxdpavelxx'
-
-from splinter.browser import Browser
-b = browser()
-url = "http://sociopages.com/twitter"
-b.visit(url)
-b.fill('q','ufc')
-
-#6.3,6.5,6.6,6.7,11.1 python cookbook
